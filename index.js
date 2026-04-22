f53597ca4a0917d14cdc5a94939f0a233ba4cb84
EnderFlop <enderflop@gmail.com>
2026-04-21 20:03:29 -0500

    proof of concept

--- index.js
+++ index.js
@@ -2,31 +2,141 @@ window.addEventListener('DOMContentLoaded', () => {
   WIDTH = window.innerWidth
   HEIGHT = window.innerHeight
 
-  setup()
-  mainLoop()
+  main()
 })
 
-function setup() {
-  // get possible cell states
-  const startingTransforms = generateTransforms()
-
-  // initialize grid of cells
-  const width = 11
+async function main() {
+  const width = 23
   const height = 11
-  const grid = initCells(width, height, startingTransforms)
-  const html = initHtml(width, height)
+  const grid = new Grid(width, height)
 
-  // observe center cell
-  const centerCell = grid[Math.floor(height / 2)][Math.floor(width / 2)]
-  centerCell.observe()
+  grid.observeCell(grid.cells[Math.floor(height / 2)][Math.floor(width / 2)])
 
-
-  //  while any cells are not yet observed (do as step?)
-  //  find cell with lowest entropy (optimize: keep track of neighbor list so we don't scan shitty nodes over and over, or sort a list, or something)
+  while (grid.unobservedCellCount > 0) {
+    await new Promise(resolve => setTimeout(resolve, 100))
+    grid.step()
+  }
 }
 
-function mainLoop() {
-  // get most likely next cell
+
+
+class Grid {
+  constructor(width, height) {
+    this.width = width
+    this.height = height
+    this.transforms = generateTransforms()
+    this.cells = this.initCells()
+    this.unobservedCellCount = width * height
+    this.initHtml()
+  }
+
+  initCells() {
+    const cells = []
+    for (let h = 0; h < this.height; h++) {
+      cells.push([])
+      for (let w = 0; w < this.width; w++) {
+        cells[h].push(new Cell(w, h, this.transforms))
+      }
+    }
+    return cells
+  }
+
+  initHtml() {
+    const container = document.getElementById("container")
+    container.style.setProperty("--cols", this.width)
+    container.style.setProperty("--rows", this.height)
+
+    for (let h = 0; h < this.height; h++) {
+      for (let w = 0; w < this.width; w++) {
+        const cell = document.createElement("div")
+        cell.id = `cell-${w}-${h}`
+        cell.className = "cell"
+        cell.style.backgroundColor = (w + h) % 2 == 0 ? `rgb(96, 56, 242)` : `rgb(119, 84, 247)`
+        cell.innerText = "@"
+        container.appendChild(cell)
+      }
+    }
+  }
+
+  step() {
+    const cell = this.getMostLikelyCell()
+    if (!cell) return
+    this.observeCell(cell)
+  }
+
+  observeCell(cell) {
+    cell.observe()
+    console.log(cell.state)
+    this.propagateConstraints(cell)
+    this.unobservedCellCount--
+    this.renderHtml()
+  }
+
+
+  getMostLikelyCell() {
+    let bestCells = []
+    let bestScore = Infinity
+    for (let h = 0; h < this.height; h++) {
+      for (let w = 0; w < this.width; w++) {
+        const cell = this.cells[h][w]
+        if (cell.observed) continue
+        const score = cell.possibleTransforms.length
+        if (score < bestScore) {
+          bestScore = score
+          bestCells = [cell]
+        } else if (score === bestScore) {
+          bestCells.push(cell)
+        }
+      }
+    }
+    const bestCell = bestCells[Math.floor(Math.random() * bestCells.length)]
+    return bestCell
+  }
+
+  propagateConstraints(cell) {
+    const { state, x, y } = cell
+    if (y > 0) {
+      const cellDown = this.cells[y - 1][x]
+      if (!cellDown.observed)
+        cellDown.possibleTransforms = cellDown.possibleTransforms.filter(t => t.down === state.up)
+    }
+    if (y < this.height - 1) {
+      const cellUp = this.cells[y + 1][x]
+      if (!cellUp.observed)
+        cellUp.possibleTransforms = cellUp.possibleTransforms.filter(t => t.up === state.down)
+    }
+    if (x > 0) {
+      const cellLeft = this.cells[y][x - 1]
+      if (!cellLeft.observed)
+        cellLeft.possibleTransforms = cellLeft.possibleTransforms.filter(t => t.right === state.left)
+    }
+    if (x < this.width - 1) {
+      const cellRight = this.cells[y][x + 1]
+      if (!cellRight.observed)
+        cellRight.possibleTransforms = cellRight.possibleTransforms.filter(t => t.left === state.right)
+    }
+  }
+
+  getNeighbors(cell) {
+    const neighbors = []
+    const x = cell.x
+    const y = cell.y
+    if (y > 0) neighbors.push(this.cells[y - 1][x])
+    if (y < this.height - 1) neighbors.push(this.cells[y + 1][x])
+    if (x > 0) neighbors.push(this.cells[y][x - 1])
+    if (x < this.width - 1) neighbors.push(this.cells[y][x + 1])
+    return neighbors
+  }
+
+  renderHtml() {
+    for (let h = 0; h < this.height; h++) {
+      for (let w = 0; w < this.width; w++) {
+        const cell = this.cells[h][w]
+        const div = document.getElementById(`cell-${w}-${h}`)
+        div.innerText = cell.state.boxChar
+      }
+    }
+  }
 }
 
 class Cell {
@@ -35,7 +145,7 @@ class Cell {
     this.y = y
     this.possibleTransforms = startingTransforms
     this.observed = false
-    this.state = null;
+    this.state = this.possibleTransforms[0];
   }
 
   // needs function that scans neighbors and prunes possibleTransforms
@@ -44,6 +154,7 @@ class Cell {
   observe() {
     this.observed = true
     this.state = this.possibleTransforms[Math.floor(Math.random() * this.possibleTransforms.length)]
+    return this.state
   }
 }
 
@@ -67,20 +178,20 @@ class Transform {
   getBoxChar(encoding) {
     const boxMap = {
       "0000": ' ',
-      "0001": '╶', // left
+      "0001": '╴', // left
       "0010": '╷', // down
-      "0011": '└',
-      "0100": '╴', // right
+      "0011": '┐',
+      "0100": '╶', // right
       "0101": '─',
       "0110": '┌',
-      "0111": '├',
+      "0111": '┬',
       "1000": '╵', // up
       "1001": '┘',
       "1010": '│',
-      "1011": '┴',
-      "1100": '┐',
-      "1101": '┤',
-      "1110": '┬',
+      "1011": '┤',
+      "1100": '└',
+      "1110": '├',
+      "1101": '┴',
       "1111": '┼'
     };
 
@@ -96,35 +207,4 @@ function generateTransforms() {
   }
 
   return transforms
-}
-
-function initCells(width, height, startingTransforms) {
-  const grid = []
-
-  for (let h = 0; h < height; h++) {
-    grid.push([])
-    for (let w = 0; w < width; w++){
-      grid[h].push(new Cell(w, h, startingTransforms))
-    }
-  }
-
-  return grid
-}
-
-function initHtml(width, height) {
-  const container = document.getElementById("container")
-
-  for (let h = 0; h < height; h++) {
-    const row = document.createElement("div")
-    row.id = "row"
-    container.appendChild(row)
-    for (let w = 0; w < width; w++){
-      const cell = document.createElement("div")
-      cell.id = "cell"
-      cell.style.backgroundColor = (w + h) % 2 == 0 ?  `rgb(96, 56, 242)` : `rgb(119, 84, 247)`
-      cell.innerText = "@"
-      row.appendChild(cell)
-    }
-  }
-
 }
